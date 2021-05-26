0ddae8bb21ef17c5b5846e4e238e85a8295c5cca
Vineeth Venudasan <vineethv@thoughtworks.com>
2021-05-26 17:13:41 +0200

    Moving some functions to companion object, as they are static in nature

--- src/main/scala/com/tw/energy/service/PricePlanService.scala
+++ src/main/scala/com/tw/energy/service/PricePlanService.scala
@@ -3,11 +3,21 @@ package com.tw.energy.service
 
 import com.tw.energy.domain.StringTypes.{PlanName, SmartMeterId}
 import com.tw.energy.domain.{ElectricityReading, PricePlan}
+import com.tw.energy.service.PricePlanService.calculateCost
 import squants.energy.{Energy, Kilowatts, Power}
 import squants.market.{EUR, Money}
 import squants.time.{Hours, Time}
 
 class PricePlanService(pricePlans: Seq[PricePlan], meterReadingService: MeterReadingService) {
+
+  def consumptionCostByPricePlan(smartMeterId: SmartMeterId): Option[Map[PlanName, Money]] = {
+    meterReadingService.getReadings(smartMeterId).map { readings =>
+      pricePlans.map(plan => plan.planName -> calculateCost(readings, plan)).toMap
+    }
+  }
+}
+
+object PricePlanService {
 
   private def calculateAverageReading(readings: Seq[ElectricityReading]): Power = {
     Kilowatts(readings.map(_.reading.toKilowatts).sum / readings.length)
@@ -19,16 +29,12 @@ class PricePlanService(pricePlans: Seq[PricePlan], meterReadingService: MeterRea
     Hours(java.time.Duration.between(first.time, last.time).getSeconds / 3600.0)
   }
 
-  private def calculateCost(readings: Seq[ElectricityReading], plan: PricePlan): Money = {
+  def calculateCost(readings: Seq[ElectricityReading], plan: PricePlan): Money = {
     val average: Power = calculateAverageReading(readings)
     val timeElapsed: Time = calculateTimeElapsed(readings)
     val energyConsumption : Energy = average * timeElapsed
     energyConsumption * plan.unitRate
   }
-
-  def consumptionCostByPricePlan(smartMeterId: SmartMeterId): Option[Map[PlanName, Money]] = {
-    meterReadingService.getReadings(smartMeterId).map { readings =>
-      pricePlans.map(plan => plan.planName -> calculateCost(readings, plan)).toMap
-    }
-  }
 }
+
+
