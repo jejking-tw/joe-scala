81fe3d2674e2a42f15e6d009eef047202a672d13
John King <jejking@github.com>
2021-06-17 20:55:54 +0200

    WIP - tests run, returns empty sequence if file exists

--- src/main/scala/com/tw/energy/repository/FileMeterReadingRepository.scala
+++ src/main/scala/com/tw/energy/repository/FileMeterReadingRepository.scala
@@ -1,11 +1,18 @@
 package com.tw.energy.repository
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
 import com.tw.energy.domain.{ElectricityReading, MeterReadings}
 import com.tw.energy.domain.StringTypes.SmartMeterId
 
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
 
 class FileMeterReadingRepository(private val path: Path) extends MeterReadingRepository {
-  override def getReadings(smartMeterId: SmartMeterId): Option[Seq[ElectricityReading]] = Option.empty
+  override def getReadings(smartMeterId: SmartMeterId): Option[Seq[ElectricityReading]] = {
+    IO {
+      val meterFilePath = path.resolve(smartMeterId)
+      if (Files.exists(meterFilePath)) Some(Seq.empty) else None
+    }.unsafeRunSync()
+  }
   // TODO - look for a file with the name of the smartMeterId underneath our directory path
   // open it, read all the lines
   // map each line to an electricity reading
--- src/test/scala/com/tw/energy/repository/FileMeterReadingRepositoryTest.scala
+++ src/test/scala/com/tw/energy/repository/FileMeterReadingRepositoryTest.scala
@@ -4,7 +4,8 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import java.nio.file.{Files, Path}
+import java.nio.file.{Files, Path, Paths}
+import java.util.Comparator
 
 class FileMeterReadingRepositoryTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
 
@@ -16,7 +17,10 @@ class FileMeterReadingRepositoryTest extends AnyFlatSpec with Matchers with Befo
   }
 
   override def afterAll(): Unit = {
-    Files.delete(tempDir)
+    Files.walk(tempDir)
+      .sorted(Comparator.reverseOrder[Path]())
+      .map(_.toFile)
+      .forEach(file => file.delete())
   }
 
   "repository" should "return None if no file for the meter id can be located" in {
@@ -29,6 +33,7 @@ class FileMeterReadingRepositoryTest extends AnyFlatSpec with Matchers with Befo
 
   it should "return an empty sequence if the file exists but is empty" in {
     val fileBasedRepository = new FileMeterReadingRepository(tempDir)
+    Files.createFile(tempDir.resolve("meter-2"))
 
     val ret = fileBasedRepository.getReadings("meter-2")
 
