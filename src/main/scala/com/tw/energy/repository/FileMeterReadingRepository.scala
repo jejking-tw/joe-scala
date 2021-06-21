d1d6e28b84647d65d841baa21fb8b011b6900574
John King <jejking@thoughtworks.com>
2021-06-21 17:50:54 +0200

    adds ability to parse the meter reading files

--- src/main/scala/com/tw/energy/repository/FileMeterReadingRepository.scala
+++ src/main/scala/com/tw/energy/repository/FileMeterReadingRepository.scala
@@ -3,14 +3,22 @@ import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import com.tw.energy.domain.{ElectricityReading, MeterReadings}
 import com.tw.energy.domain.StringTypes.SmartMeterId
+import com.tw.energy.repository.FileMeterReadingRepository.parseLine
+import squants.energy.Kilowatts
 
 import java.nio.file.{Files, Path}
+import java.time.Instant
+import scala.jdk.CollectionConverters._
 
 class FileMeterReadingRepository(private val path: Path) extends MeterReadingRepository {
   override def getReadings(smartMeterId: SmartMeterId): Option[Seq[ElectricityReading]] = {
     IO {
       val meterFilePath = path.resolve(smartMeterId)
-      if (Files.exists(meterFilePath)) Some(Seq.empty) else None
+      if (Files.exists(meterFilePath)) {
+        Some(Files.readAllLines(meterFilePath).asScala.map(parseLine(_)).toSeq)
+      } else {
+        None
+      }
     }.unsafeRunSync()
   }
   // TODO - look for a file with the name of the smartMeterId underneath our directory path
@@ -21,4 +29,18 @@ class FileMeterReadingRepository(private val path: Path) extends MeterReadingRep
   // and if the parsing fails.... what then?
 
   override def storeReadings(meterReadings: MeterReadings): Unit = ???
+
+
+
+}
+
+object FileMeterReadingRepository {
+
+  val line = raw"(.+),(.+)".r
+
+  def parseLine(input: String): ElectricityReading = {
+    input match {
+      case line (instant, power) => ElectricityReading(Instant.ofEpochSecond(instant.toLong), Kilowatts(power.toDouble))
+    }
+  }
 }
--- src/test/scala/com/tw/energy/repository/FileMeterReadingRepositoryTest.scala
+++ src/test/scala/com/tw/energy/repository/FileMeterReadingRepositoryTest.scala
@@ -1,10 +1,14 @@
 package com.tw.energy.repository
 
+import com.tw.energy.domain.ElectricityReading
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import squants.energy.Kilowatts
 
+import java.io.{BufferedWriter, FileWriter}
 import java.nio.file.{Files, Path, Paths}
+import java.time.Instant
 import java.util.Comparator
 
 class FileMeterReadingRepositoryTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll {
@@ -39,4 +43,23 @@ class FileMeterReadingRepositoryTest extends AnyFlatSpec with Matchers with Befo
 
     ret shouldBe Option(Seq.empty)
   }
+
+  it should "return a sequence of one electricity reading given a file with one correctly formatted entry" in {
+    val fileBasedRepository = new FileMeterReadingRepository(tempDir)
+    val meter3Path = Files.createFile(tempDir.resolve("meter-3"))
+    Files.write(meter3Path, "1624289430,1234.56".getBytes())
+
+    val expectedElectrityReading = ElectricityReading(Instant.ofEpochSecond(1624289430), Kilowatts(1234.56))
+
+    val ret = fileBasedRepository.getReadings("meter-3")
+    ret shouldBe Some(Seq(expectedElectrityReading))
+  }
+
+  "the line parser" should "parse a string representing a line to an electricity reading" in {
+    val input = "1624289430,1234.56"
+    val expectedElectrityReading = ElectricityReading(Instant.ofEpochSecond(1624289430), Kilowatts(1234.56))
+
+    FileMeterReadingRepository.parseLine(input) shouldBe expectedElectrityReading
+  }
+
 }
